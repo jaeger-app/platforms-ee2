d3d665b9f84abb0153b1558fb98dde948b300f72
Eric Lamb <eric@mithra62.com>
2016-04-15 13:37:23 -0700

    Initial import

---
+++ src/View/Eecms.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Jaeger
+ *
+ * @copyright	Copyright (c) 2015-2016, mithra62
+ * @link		http://jaeger-app.com
+ * @version		1.0
+ * @filesource 	./Platforms/Eecms.php
+ */
+namespace JaegerApp\View\Eecms;
+
+/**
+ * Jaeger - Eecms View Object
+ *
+ * Contains the view helpers for ExpressionEngine
+ *
+ * @package Platforms\Eecms
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Eecms extends AbstractView
+{
+    
+}
